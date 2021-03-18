82c41758ccbeafa625733858b8174ed7832f0f05
Victor Nieves <victor.nieves@inetum.world>
2021-03-18 12:46:15 +0100

    removed some comments

--- truffle-config.js
+++ truffle-config.js
@@ -42,10 +42,10 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    quorum: {
-      host: "127.0.0.1",      // Localhost (default: none)
-      port: 22000,            // Standard Ethereum port (default: none)
-      network_id: "*",        // Any network (default: none)
+    quorum: {               // Quorum test node
+      host: "127.0.0.1",
+      port: 22000,
+      network_id: "*",
       type:"quorum",
       gasPrice: 0
       //from: ""
